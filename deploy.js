d8a0117166c0b31e7207eebfeca70e7eedd013cb
Victor Han <sunmoon11@yandex.ru>
2021-07-31 15:52:22 +0500

    Update deploy.js

--- deploy.js
+++ deploy.js
@@ -1,1 +1,22 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const {abi, bytecode} = require('./compile.js');
+const mnemonic = require('./MNEMONIC.js')
+const provider = new HDWalletProvider(
+    mnemonic,
+    'https://rinkeby.infura.io/v3/5f63e1484039412f87d13fbc5edd262f'
+);
+const web3 = new Web3(provider); 
 
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    console.log("Attempting to deploy from account", accounts[0]);
+
+    const result = await new web3.eth.Contract(JSON.parse(abi))
+        .deploy({data: bytecode, arguments: ["Hi there!"]})
+            .send({gas: "1000000", from: accounts[0]})
+    console.log("Contract deployed to", result.options.address);
+};
+
+deploy();
